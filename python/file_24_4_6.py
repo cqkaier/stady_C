29d7db61aca225a9b4ebfed2fd13555af4144d5c
YouL <cqkaier@proton.me>
2024-04-06 22:59:40 +0800

    windwons commit 24_4_6

--- python/file_24_4_6.py
+++ python/file_24_4_6.py
@@ -1,3 +1,5 @@
+import keyword
+import random
 luck_number=8
 my_name='cqkaier'
 print('幸运树枝是:',luck_number)
@@ -20,3 +22,45 @@ for i in range(100,1000):
     hundred=i//100
     if sd**3+ten**3+hundred**3==i:
         print('100-999之间的水仙花数为:',i)
+lst=["hello","world","python","sql"]
+print(lst[2],"内存地址:",id(lst))
+#增加元素
+lst.append("docker")
+print("增加元素之后:",lst,"内存地址:",id(lst))
+
+lst2=[4,56,3,78,40,56,89]
+print('原列表:',lst2)
+lst2.sort()
+print('升序:',lst2)
+lst2.sort(reverse=True)
+print('降序:',lst2)
+print(keyword.kwlist)
+lst3=[itme for itme in range(1,11)]
+print(lst3)
+lst3=[itme*itme for itme in range(1,11)]
+print(lst3)
+lst3=[random.randint(1,100) for _ in range(10)]
+print(lst3)
+lst4=[i for i in range(10) if i%2==0]
+print(lst4)
+#二维列表的遍历与列表生成
+lst5=[
+    ["城市","环比","同比"],
+    ["北京",102,103],
+    ["上海",104,504],
+    ["深圳",100,39]
+]
+print(lst5)
+#二维列表遍历使用双层for循环
+for row in lst5:#行
+    for rew2 in row:#列
+        print(rew2,end="\t")
+    print()
+#列表生成式
+lst6=[[j for j in range(5)]for i in range(4)]
+print(lst6)
+for row1 in lst6:
+    for sow1 in row1:
+        print(sow1,end="\t")
+    print()
+#元组
