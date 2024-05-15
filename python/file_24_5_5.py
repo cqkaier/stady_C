61532e93c54baff8e5442d90f9f65d0039731b83
YouL <cqkaier@proton.me>
2024-05-15 12:20:50 +0800

    commit in 24_5_15

--- python/file_24_5_5.py
+++ python/file_24_5_5.py
@@ -1,4 +1,5 @@
 import random
+#实战一
 def get_max(lst):
     x=lst[0]
     for i in range(1,len(lst)):
@@ -9,3 +10,35 @@ lst=[random.randint(1,100) for item in range(10)]
 print(lst)
 max=get_max(lst)
 print(max)
+print('-'*30)
+#实战二
+def get_digit(x):
+    s=0
+    lst=[]
+    for item in x:
+        if item.isdigit():
+            lst.append(int(item))
+    s=sum(lst)
+    return lst,s
+s=input('请输入一个加有数字的字符串:')
+lst,x=get_digit(s)
+print(f'获取的数字列表为:{lst}')
+print(f'累加和为:{x}')
+print('-'*30)
+
+#实战三大小写转换
+def swap(s):
+    lst1=[]
+    for item in s:
+        if 'A'<=item<='Z':
+            lst1.append(chr(ord(item)+32))#ord将字符转成unicode码，加上32转为小写，char转为字符
+        elif 'a'<=item<='z':
+            lst1.append(chr(ord(item)-32))
+        else:
+            lst1.append(item)
+    return ''.join(lst1)#  ''.join将列表进行拼接
+a=input('please enter some string:')
+news=swap(a)
+print(news)
+print('-'*30)
+#实战四
--- python/test.py
+++ python/test.py
@@ -1,2 +1,6 @@
 name = input('请输入你的名字：')
 print('您的名字是：'+name)
+lst=['hell0','world']
+print(''.join(lst))#  ''.join将列表进行拼接
+a='aHpvY2JlTG1namdJR05vUVpzSG9seFdCbzMzcVYyaU5QNjIwZWtmL0Yvd0ZoSzV1QmxUdGZjdGFnZHo3dlVwc0FnQjJjbklrYU9XQTBsdkFDeHd0b1FXNGlMNDFSVEcvZFhzQ09lZjE2R289Cg=='
+print(str(len(a)))
